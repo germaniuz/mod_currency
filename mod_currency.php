2233c8320b43cc05967cd8893790781ebb3c03c3
German <germanius@list.ru>
2016-02-25 17:24:03 +0300

    Curency view ready

--- mod_currency.php
+++ mod_currency.php
@@ -1,7 +1,11 @@
-<?
-
+<? 
+ // defined( '_JEXEC' ) or die( 'Restricted access' );
+  
 // Переменные
-$url = 'https://query.yahooapis.com/v1/public/yql?q=select+*+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+$url = 'https://query.yahooapis.com/v1/public/yql?q=select+Rate+from+yahoo.finance.xchange+where+pair+=+%22USDRUB,EURRUB%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=';
+// url for C Volgograd
+// https://query.yahooapis.com/v1/public/yql?q=select%20item.long%20from%20weather.forecast%20where%20woeid%3D2124298%0A&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys
+// url for brent oil https://query.yahooapis.com/v1/public/yql?q=select%20DaysHigh%20from%20yahoo.finance.quote%20where%20symbol%20in%20(%22BZM16.NYM%22)&format=json&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
 $cache = 'cache/rates.cache';
 
 // Функция получения JSON с помощью cURL
@@ -39,9 +43,11 @@ if (file_exists($cache) && $file_time <= 86400){
   $data = json_decode(read_cache($cache));
 };
 // Записываем курсы в переменные
-$usd_rate = $data->query->results->rate[0]->Rate;
-echo $usd_rate;
-$eur_rate = $data->query->results->rate[1]->Rate;
-echo $eur_rate;
+$usd_rate = substr($data->query->results->rate[0]->Rate, 0, -2);
+$eur_rate = substr($data->query->results->rate[1]->Rate, 0, -2);
+//Считаем разницу в курсах
+// $usd_diff = $old_usd_rate - $usd_rate;
+// $eur_diff = $old_eur_rate - $eur_rate;
 
+require(JmoduleHelper::getLayoutPath('mod_currency'));
 ?>
---
+++ tmpl/default.php
@@ -0,0 +1,8 @@
+<p>
+  <!-- TODO Подключить скрипты температуры -->
+  Волгоград <span class="temperature">-3&deg;C</span>
+  <!-- TODO Подключить разницу курсов -->
+  | USD - <?= $usd_rate; ?> <span class="curency-up">0.48</span>  EUR - <?= $eur_rate;  ?> <span class="curency-down">0.68</span>
+  <!-- TODO Посмотреть лучшую реализацию -->
+  <span class="desktop">НЕФТЬ - 32,42 <span class="curency-down">0.41%</span></span>
+</p>
